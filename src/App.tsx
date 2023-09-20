9930c3d0810e591a52c61e824e346d0de94d25d3
alekseiveprev <72622572+alekseiveprev@users.noreply.github.com>
2023-09-20 10:00:07 +0300

    init: start HW

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import { useState } from 'react';
 import { FC } from 'react';
 
 import './style.css';
 
 export const App: FC<{ name: string }> = ({ name }) => {
+  let [a, setA] = useState(1);
+
+  const onClickHandler = () => {
+    setA(++a);
+    console.log(a);
+  };
+
+  let [b = setA, setB] = useState();
+  const resetAHandler = () => {
+    //setB(b = 0);
+    console.log(b);
+  };
+
+  /* let [b, setB] = useState(a);
+  const resetAHandler = () => {
+     setB((a = 0));
+    console.log(b);
+  }; */
+
   return (
     <div>
-      <h1>Hello {name}!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>{a}</h1>
+      <button onClick={onClickHandler}>number</button>
+      <button onClick={resetAHandler}>0</button>
     </div>
   );
 };
